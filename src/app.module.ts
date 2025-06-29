cb5f92485af34175aa03cf374a665452a36a42cc
Nicole Cruz <10cruznicole@gmail.com>
2025-06-29 06:31:18 -0400

    conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,13 +12,8 @@ import { ScholarshipModule } from './modules/scholarship/scholarship.module';
   controllers: [AppController],
   providers: [AppService],
   imports: [CvParserModule,RecommenderModule, ConfigModule.forRoot({ // <-- Add this line
-      isGlobal: true, // Makes the ConfigService available globally
-<<<<<<< HEAD
+    isGlobal: true, // Makes the ConfigService available globally
     }),FirebaseModule, ScholarshipModule,],
-  controllers: [],
-  providers: [],
-=======
-    }),FirebaseModule,],
->>>>>>> 5f13c01af4f9b3691ee88b067cbd6f7a32df86e1
 })
+
 export class AppModule {}
