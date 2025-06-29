198be07199170554d98c263b2b80313bed913305
Jessica Rodriguez Guarayo <JessRod2000@users.noreply.github.com>
2025-06-29 00:13:26 -0400

    Firebase CV storage setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CvParserModule } from './modules/cv-parser/cv-parser.module';
 
-//import { FirebaseModule } from './firebase/firebase.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [CvParserModule, ConfigModule.forRoot({ // <-- Add this line
       isGlobal: true, // Makes the ConfigService available globally
-    }),],
+    }),FirebaseModule,],
   controllers: [],
   providers: [],
 })
